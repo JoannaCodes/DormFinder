dd60978869165c0d6be332e84e1d26a09af81fb9
JoannaCodes <joannalara21@gmail.com>
2023-03-13 17:13:22 +0800

    Update DormListing.js

--- src/components/profileScreen/DormListing.js
+++ src/components/profileScreen/DormListing.js
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React from 'react';
 
 const DormListing = () => {
   return (
     <View>
       <Text>DormListing</Text>
     </View>
-  )
-}
+  );
+};
 
-export default DormListing
+export default DormListing;
