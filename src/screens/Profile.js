5b99590134bb175b2d4f862a826aa0319e06d386
JoannaCodes <joannalara21@gmail.com>
2023-04-24 12:12:03 +0800

    Accepted Current Change

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,18 +1,10 @@
 /* eslint-disable react-native/no-inline-styles */
-<<<<<<< HEAD
 import React,{useState,useEffect} from 'react'
 import {Modal,KeyboardAvoidingView,View,Text,AsyncStorage,SafeAreaView,StyleSheet,Button,Image,Alert, ToastAndroid, TouchableOpacity,TextInput,FlatList,ScrollView} from 'react-native'
 import {initialStyles} from '../styles/initial'
 import axios from 'axios'
 import {BASE_URL} from '../../constants'
 import DocumentPicker from 'react-native-document-picker';
-=======
-import {StyleSheet, View, Text, ScrollView} from 'react-native';
-import React from 'react';
-import {PrimaryBtn, TertiaryBtn} from '../components/others/Buttons';
-
-const Separator = () => <View style={styles.separator} />;
->>>>>>> 94c74105714618eb06f88c02cb22b21d25a023e2
 
 export default function Profile({navigation}) {
   async function _sendDocument() {
@@ -57,7 +49,6 @@ export default function Profile({navigation}) {
   }
   }
   return (
-<<<<<<< HEAD
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       
       <Button
@@ -80,104 +71,7 @@ export default function Profile({navigation}) {
         title="Send Document"
         onPress={_sendDocument}
       />
-=======
-    <View style={styles.container}>
-      <ScrollView
-        contentContainerStyle={styles.scrollView}
-        showsVerticalScrollIndicator={false}>
-        <View style={styles.section}>
-          <Text style={styles.header}>{uid}</Text>
-          <Text style={styles.label}>Email</Text>
-          <Text style={styles.label}>Contact Number</Text>
-        </View>
-
-        <View style={styles.section}>
-          <Text style={styles.label}>Profile Settings</Text>
-          <PrimaryBtn
-            title="Edit Profile"
-            onPress={() => navigation.navigate('Edit Profile', {userId: uid})}
-            buttonColor="#FFFFFF"
-            buttonStyle={{marginBottom: 10}}
-          />
-          <PrimaryBtn
-            title="Change Password"
-            onPress={() =>
-              navigation.navigate('Change Password', {userId: uid})
-            }
-            buttonColor="#FFFFFF"
-          />
-        </View>
-
-        <View style={styles.section}>
-          <Text style={styles.label}>Listing Settings</Text>
-          <PrimaryBtn
-            title="Dorm Listing"
-            onPress={() => navigation.navigate('Dorm Listing', {userId: uid})}
-            buttonColor="#FFFFFF"
-            buttonStyle={{marginBottom: 10}}
-          />
-          <PrimaryBtn
-            title="Verification"
-            onPress={() => navigation.navigate('Verification', {userId: uid})}
-            buttonColor="#FFFFFF"
-            buttonStyle={{marginBottom: 10}}
-          />
-          <PrimaryBtn
-            title="Payments"
-            onPress={() => navigation.navigate('Payments', {userId: uid})}
-            buttonColor="#FFFFFF"
-          />
-        </View>
-
-        <View style={styles.section}>
-          <Separator />
-          <TertiaryBtn title="About Dorm Finder" textColor="#000000" />
-          <Separator />
-          <TertiaryBtn title="Contact Us" textColor="#000000" />
-          <Separator />
-        </View>
-
-        <View style={styles.section}>
-          <PrimaryBtn
-            title="Logout"
-            buttonStyle={{height: 45}}
-            textStyle={{textAlign: 'center'}}
-            textColor="#FFFFFF"
-          />
-        </View>
-      </ScrollView>
->>>>>>> 94c74105714618eb06f88c02cb22b21d25a023e2
     </View>
   );
 
-<<<<<<< HEAD
 };
-=======
-export default Profile;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  scrollView: {
-    flexGrow: 1,
-    justifyContent: 'space-between',
-  },
-  section: {
-    padding: 16,
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'black',
-    marginBottom: 5,
-  },
-  label: {
-    fontSize: 16,
-  },
-  separator: {
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-});
->>>>>>> 94c74105714618eb06f88c02cb22b21d25a023e2
