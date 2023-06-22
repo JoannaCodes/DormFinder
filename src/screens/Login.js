9054b588425bb3107ffc992dee4332b383ac7975
Tricia Gilo <gilotriciac@gmail.com>
2023-06-22 22:26:13 +0800

    tricia 6/22/23

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -10,6 +10,7 @@ import {
   TouchableOpacity,
   Alert,
   ActivityIndicator,
+  ScrollView,
 } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import axios from 'axios';
@@ -19,7 +20,7 @@ import COLORS from '../../constants/colors';
 
 import BackgroundImg from '../../assets/img/bg1.png';
 import Google from '../../assets/img/google-logo.png';
-// import {BASE_URL} from '../../constants/index';
+import {BASE_URL} from '../../constants/index';
 import ForgotPasswordModal from '../components/modals/ForgotPasswordModal';
 import { API_URL, AUTH_KEY, CLIENT_ID } from '../../constants/index';
 
@@ -199,6 +200,7 @@ export default function Login({onLogin}) {
   };
 
   return (
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
     <View style={styles.container}>
       <StatusBar hidden={true} />
       <View style={styles.topBackgroundImgContainer}>
@@ -345,6 +347,7 @@ export default function Login({onLogin}) {
         onClose={() => setModalVisible(false)}
       />
     </View>
+    </ScrollView>
   );
 }
 
@@ -415,5 +418,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginVertical: 10,
     borderRadius: 10,
+  },
+  scrollContainer: {
+    flexGrow: 1,
   },
 });
